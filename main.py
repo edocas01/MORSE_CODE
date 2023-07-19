52216014c10ab39db86f57612f787d85dad4e61c
edoardo <edoardo.castioni@gmail.com>
2023-07-19 15:31:03 +0200

    updating code

--- main.py
+++ main.py
@@ -2,14 +2,17 @@
 import random
 
 """
-   ____        __ _       _ _   _                 
-  |  _ \  ___ / _(_)_ __ (_) |_(_) ___  _ __  ___ 
-  | | | |/ _ \ |_| | '_ \| | __| |/ _ \| '_ \/ __|
-  | |_| |  __/  _| | | | | | |_| | (_) | | | \__ \
-  |____/ \___|_| |_|_| |_|_|\__|_|\___/|_| |_|___/
-                                                  
+ 
+   _____                 _   _                 
+  |  ___|   _ _ __   ___| |_(_) ___  _ __  ___ 
+  | |_ | | | | '_ \ / __| __| |/ _ \| '_ \/ __|
+  |  _|| |_| | | | | (__| |_| | (_) | | | \__ \
+  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
+                                               
  
 """
+
+# this function reads the file and returns the letters, codes and names
 def read_file(file_name):
     alphabet = open(file_name, "r").read()
     letters = []
@@ -28,6 +31,8 @@ def read_file(file_name):
             break
     return letters, codes, names
 
+
+# this function requests a letter and checks if the code is correct
 def request_letter(l,c,n):
     if len(l) != len(c) or len(l) != len(n):
         return False
@@ -41,10 +46,37 @@ def request_letter(l,c,n):
         print("Incorrect! The correct answer was " + c[letter])
         False
 
-
-
+# this function checks if the code associated to a given phrase is correct
+def check_phrase(l = None, c = None):
+    print("=> EXERCISES ON MORSE CODE")
+    print("- press \"exit\" to exit")
+    print("- space the letters in morse code with one space")
+    print("- space the words in morse code with two spaces")
+    print("==============================================")
+    exit_str = "exit"
+    while True:
+        text_phrase = input("Give the phrase to be checked:\n")
+        text_phrase = text_phrase.upper()
+        if text_phrase == exit_str.upper():
+            break
+        # parsing the phrase
+        words = text_phrase.split(" ")
+        new_words = []
+        for x in words:
+            if x != "":
+                new_words.append(x)
+        words = new_words
+        print(words)
+        # give as input the corresponding morse code and check if it is correct
+        check = True
+        while check:
+            code_phrase = input("Give the corresponding morse code:\n")
+            for i in range(len(words)):
+                for j in range(len(words[i])):
+                    if words[i][j] != ..... # TO BE CONTINUED
+            
+            check = False
 """
- 
    __  __       _       
   |  \/  | __ _(_)_ __  
   | |\/| |/ _` | | '_ \ 
@@ -53,8 +85,10 @@ def request_letter(l,c,n):
                         
  
 """
+
 letters, codes, names = read_file("alfabeto.txt")
-request_letter(letters,codes,names)
+check_phrase(letters, codes)
+
 
 
 
